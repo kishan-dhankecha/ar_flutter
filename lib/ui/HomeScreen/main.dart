a5d65acf7021393c981b6f520187040683f9b930
Kishan-dhankecha <Dhankechakishan@gmail.com>
2021-07-18 15:22:32 +0530

    ARView added in home screen

--- lib/ui/HomeScreen/main.dart
+++ lib/ui/HomeScreen/main.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'home';
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ArCoreController arCoreController;
+
+  _onArCoreViewCreated(ArCoreController _arCoreController) {
+    arCoreController = _arCoreController;
+    _addCube(arCoreController);
+  }
+
+  _addCube(ArCoreController _arCoreController) {
+    final material = ArCoreMaterial(color: Colors.white, metallic: 1);
+    final cube =
+        ArCoreCube(materials: [material], size: vector.Vector3(0.5, 0.5, 0.5));
+    final node = ArCoreNode(
+      shape: cube,
+      position: vector.Vector3(0.5, -3, -3),
+    );
+
+    _arCoreController.addArCoreNode(node);
+  }
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: Text("AR FLUTTER"),
+        centerTitle: true,
+      ),
+      body: ArCoreView(onArCoreViewCreated: _onArCoreViewCreated),
     );
   }
 }
